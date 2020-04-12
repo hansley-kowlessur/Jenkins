7b89de5faae1009af70a7c7a70c166d7702ce236
Hansley <hansleykowlessur1998@gmail.com>
2020-04-12 14:30:53 +0400

    first commit

---
+++ Jenkins.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author Admin
+ */
+import java.util.*;
+public class Jenkins {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Date dat = new java.util.Date();
+        System.out.println("Date = "+dat);
+    }
+    
+}
